d9eb2528aa6f286c5e8dd356fecb54dec72034f9
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-08-02 21:41:28 -0600

    Started ocr-numbers
    Created keytype enum and number struct, and defined all numbers

--- rust/ocr-numbers/src/lib.rs
+++ rust/ocr-numbers/src/lib.rs
@@ -7,6 +7,187 @@ pub enum Error {
     InvalidColumnCount(usize),
 }
 
+pub enum KeyType {
+    Pipe,
+    Underscore,
+    Space,
+}
+
+pub struct Number {
+    top_left: KeyType,
+    top_middle: KeyType,
+    top_right: KeyType,
+    upper_middle_left: KeyType,
+    upper_middle_middle: KeyType,
+    upper_middle_right: KeyType,
+    lower_middle_left: KeyType,
+    lower_middle_middle: KeyType,
+    lower_middle_right: KeyType,
+    bottom_left: KeyType,
+    bottom_middle: KeyType,
+    bottom_right: KeyType,
+}
+
+const ONE: Number = Number {
+    top_left: KeyType::Space,
+    top_middle: KeyType::Space,
+    top_right: KeyType::Space,
+    upper_middle_left: KeyType::Space,
+    upper_middle_middle: KeyType::Space,
+    upper_middle_right: KeyType::Pipe,
+    lower_middle_left: KeyType::Space,
+    lower_middle_middle: KeyType::Space,
+    lower_middle_right: KeyType::Pipe,
+    bottom_left: KeyType::Space,
+    bottom_middle: KeyType::Space,
+    bottom_right: KeyType::Space,
+};
+
+const TWO: Number = Number {
+    top_left: KeyType::Space,
+    top_middle: KeyType::Underscore,
+    top_right: KeyType::Space,
+    upper_middle_left: KeyType::Space,
+    upper_middle_middle: KeyType::Underscore,
+    upper_middle_right: KeyType::Pipe,
+    lower_middle_left: KeyType::Pipe,
+    lower_middle_middle: KeyType::Underscore,
+    lower_middle_right: KeyType::Space,
+    bottom_left: KeyType::Space,
+    bottom_middle: KeyType::Space,
+    bottom_right: KeyType::Space,
+};
+
+const THREE: Number = Number {
+    top_left: KeyType::Space,
+    top_middle: KeyType::Underscore,
+    top_right: KeyType::Space,
+    upper_middle_left: KeyType::Space,
+    upper_middle_middle: KeyType::Underscore,
+    upper_middle_right: KeyType::Pipe,
+    lower_middle_left: KeyType::Space,
+    lower_middle_middle: KeyType::Underscore,
+    lower_middle_right: KeyType::Pipe,
+    bottom_left: KeyType::Space,
+    bottom_middle: KeyType::Space,
+    bottom_right: KeyType::Space,
+};
+
+const FOUR: Number = Number {
+    top_left: KeyType::Space,
+    top_middle: KeyType::Space,
+    top_right: KeyType::Space,
+    upper_middle_left: KeyType::Pipe,
+    upper_middle_middle: KeyType::Underscore,
+    upper_middle_right: KeyType::Pipe,
+    lower_middle_left: KeyType::Space,
+    lower_middle_middle: KeyType::Space,
+    lower_middle_right: KeyType::Pipe,
+    bottom_left: KeyType::Space,
+    bottom_middle: KeyType::Space,
+    bottom_right: KeyType::Space,
+};
+
+const FIVE: Number = Number {
+    top_left: KeyType::Space,
+    top_middle: KeyType::Underscore,
+    top_right: KeyType::Space,
+    upper_middle_left: KeyType::Pipe,
+    upper_middle_middle: KeyType::Underscore,
+    upper_middle_right: KeyType::Space,
+    lower_middle_left: KeyType::Space,
+    lower_middle_middle: KeyType::Underscore,
+    lower_middle_right: KeyType::Pipe,
+    bottom_left: KeyType::Space,
+    bottom_middle: KeyType::Space,
+    bottom_right: KeyType::Space,
+};
+
+const SIX: Number = Number {
+    top_left: KeyType::Space,
+    top_middle: KeyType::Underscore,
+    top_right: KeyType::Space,
+    upper_middle_left: KeyType::Pipe,
+    upper_middle_middle: KeyType::Underscore,
+    upper_middle_right: KeyType::Space,
+    lower_middle_left: KeyType::Pipe,
+    lower_middle_middle: KeyType::Underscore,
+    lower_middle_right: KeyType::Pipe,
+    bottom_left: KeyType::Space,
+    bottom_middle: KeyType::Space,
+    bottom_right: KeyType::Space,
+};
+
+const SEVEN: Number = Number {
+    top_left: KeyType::Space,
+    top_middle: KeyType::Underscore,
+    top_right: KeyType::Space,
+    upper_middle_left: KeyType::Space,
+    upper_middle_middle: KeyType::Space,
+    upper_middle_right: KeyType::Pipe,
+    lower_middle_left: KeyType::Space,
+    lower_middle_middle: KeyType::Space,
+    lower_middle_right: KeyType::Pipe,
+    bottom_left: KeyType::Space,
+    bottom_middle: KeyType::Space,
+    bottom_right: KeyType::Space,
+};
+
+const EIGHT: Number = Number {
+    top_left: KeyType::Space,
+    top_middle: KeyType::Underscore,
+    top_right: KeyType::Space,
+    upper_middle_left: KeyType::Pipe,
+    upper_middle_middle: KeyType::Underscore,
+    upper_middle_right: KeyType::Pipe,
+    lower_middle_left: KeyType::Pipe,
+    lower_middle_middle: KeyType::Underscore,
+    lower_middle_right: KeyType::Pipe,
+    bottom_left: KeyType::Space,
+    bottom_middle: KeyType::Space,
+    bottom_right: KeyType::Space,
+};
+
+const NINE: Number = Number {
+    top_left: KeyType::Space,
+    top_middle: KeyType::Underscore,
+    top_right: KeyType::Space,
+    upper_middle_left: KeyType::Pipe,
+    upper_middle_middle: KeyType::Underscore,
+    upper_middle_right: KeyType::Pipe,
+    lower_middle_left: KeyType::Space,
+    lower_middle_middle: KeyType::Underscore,
+    lower_middle_right: KeyType::Pipe,
+    bottom_left: KeyType::Space,
+    bottom_middle: KeyType::Space,
+    bottom_right: KeyType::Space,
+};
+
+const ZERO: Number = Number {
+    top_left: KeyType::Space,
+    top_middle: KeyType::Underscore,
+    top_right: KeyType::Space,
+    upper_middle_left: KeyType::Pipe,
+    upper_middle_middle: KeyType::Space,
+    upper_middle_right: KeyType::Pipe,
+    lower_middle_left: KeyType::Pipe,
+    lower_middle_middle: KeyType::Underscore,
+    lower_middle_right: KeyType::Pipe,
+    bottom_left: KeyType::Space,
+    bottom_middle: KeyType::Space,
+    bottom_right: KeyType::Space,
+};
+
 pub fn convert(input: &str) -> Result<String, Error> {
-    todo!("Convert the input '{input}' to a string");
+    // TODO: determine if cols and rows are correct dimensions (3cols, 4rows)
+    let input_vec = input.chars().collect::<Vec<char>>();
+    println!("{:?}", input_vec);
+
+    // TODO: iterate over str type; after every 3 chars, a new row starts
+    // TODO: add each char to the Number struct. if invalid, put a ?
+    // TODO: Encode each Number
+    //
+    //
+
+    Ok("String".to_string())
 }
